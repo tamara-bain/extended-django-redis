7cc028c07a0c232320f8b01ef975fad45b306b87
tamara-bain <tamarabyte@gmail.com>
2019-06-17 12:30:14 -0400

    update django version

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
   name='extended-django-redis',
-  version='0.22.7',
+  version='0.22.8',
   packages=find_packages(),
   include_package_data=True,
-  install_requires=['Django==2.1.3', 'django-redis==4.9.0', 'pytz==2018.7', 'redis==2.10.6'],
+  install_requires=['Django', 'django-redis==4.9.0', 'pytz', 'redis==2.10.6'],
   description='Extends the standard caching backend for Django to have additional redis features',
 )
